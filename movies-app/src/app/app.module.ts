d2f8d3505a6c0e5a4533cf46bec2f0fd68492f9d
dandaradantas <dandaranavarro@gmail.com>
2020-03-29 12:35:28 -0700

    Formatting the movies as cards

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import { MovieComponent } from './movie/movie.component';
 import { MovieService } from './movie.service'
 import { AppRoutingModule } from './app-routing.module';
@@ -17,8 +19,8 @@ import { GenresComponent } from './genres/genres.component';
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule,
-    MatTabsModule, MatSelectModule, AppRoutingModule
-  ],
+    MatTabsModule, MatSelectModule, AppRoutingModule, MatCardModule, MatPaginatorModule
+    ],
   providers: [ MovieService ],
   bootstrap: [AppComponent]
 })
--- movies-app/src/app/movie/movie.component.ts
+++ movies-app/src/app/movie/movie.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {MovieService} from '../movie.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
@@ -30,24 +28,6 @@ export class MovieComponent implements OnInit {
       this.loadMovies(routeParams.id);
     });
 
-
-    // let id = parseInt(this.activeRoute.snapshot.paramMap.get('id'));
-    // this.selectedGenre = id;
-
-
-    //location.reload(); 
-
-      // this.activeRoute.params.subscribe((params) => {
-      //   if(params['id']){
-      //     this.hasGenre = true;
-      //     const id = params['id'];
-      //     this.genreName = params['name'];
-      //     this._movieServices.getMoviesByGenre(id).subscribe(res => {
-      //       this.selectedMoviesByGenre = res.results;
-      //     });
-      //   } 
-        
-      // })
   }
 
   loadMovies(id) {
